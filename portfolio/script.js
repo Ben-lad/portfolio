f80e117ef375e19a8f65bbb2f9d54246750c3424
ladegbaye benjamin ademide <112470050+benjaminladegbayeademide@users.noreply.github.com>
2022-11-22 21:17:30 +0100

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+var text = document.getElementById("contact");
+var back = document.getElementById("contact-background");
+
+text.addEventListener("click",(e) =>{
+e.preventDefault
+back.style.visibility="visible"
+})
