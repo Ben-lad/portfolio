d4fffbbe9c0ab50ac865c2b3c9ec8a64fed6ed5a
Utsab Kundu <92643166+Utsab5@users.noreply.github.com>
2022-11-23 21:28:41 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,7 +1,22 @@
 var text = document.getElementById("contact");
 var back = document.getElementById("contact-background");
 
-text.addEventListener("click",(e) =>{
-e.preventDefault
-back.style.visibility="visible"
-})
+
+
+text.onclick=function(){
+    text.classList.toggle('active');
+    back.classList.toggle('active');
+}
+
+document.onclick = function(e){
+    if(e.target.id !== 'contact-background' && e.target.id !== 'contact')
+    {
+        text.classList.remove('active');
+        back.classList.remove('active');
+    }
+}
+
+// text.addEventListener("click",(e) =>{
+// e.preventDefault
+// back.style.visibility="visible"
+// })
