7ecd6e8f9f76846592d6d3db0de906feb06acd4e
Obawomiye Blessing <obawomiyeblessing1@gmail.com>
2022-11-25 01:32:46 +0100

    add some music file

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,22 +1,46 @@
 var text = document.getElementById("contact");
 var back = document.getElementById("contact-background");
+var text1 = document.getElementById("FAQ");
+var back1 = document.getElementById("FAQ-background");
+var music = document.querySelector("audio");
+var fre = document.querySelector(".answer1")
+var quest = document.querySelector(".question1")
+var fre = document.querySelector(".answer1")
+var btn = document.querySelector("#btn1")
 
 
 
-text.onclick=function(){
-    text.classList.toggle('active');
-    back.classList.toggle('active');
-}
 
-document.onclick = function(e){
-    if(e.target.id !== 'contact-background' && e.target.id !== 'contact')
-    {
-        text.classList.remove('active');
-        back.classList.remove('active');
+// music.play()
+
+text.onclick = function () {
+  text.classList.toggle("active");
+  back.classList.toggle("active");
+};
+
+document.onclick = function (e) {
+  if (e.target.id !== "contact-background" && e.target.id !== "contact") {
+    // text.classList.add("move")
+    text.classList.remove("active");
+    back.classList.remove("active");
+  }
+};
+
+text.onclick = function () {
+  text.classList.toggle("active");
+  back.classList.toggle("active");
+};
+
+text1.onclick = () => {
+  back1.classList.toggle("mode");
+};
+
+window.onclick = (event) => {
+  if(event.target.closest("back1"))
+  return false
+  if (!event.target.matches("#FAQ")) {
+    if (back1.classList.contains("mode")) {
+      back1.classList.remove("mode");
     }
-}
-
-// text.addEventListener("click",(e) =>{
-// e.preventDefault
-// back.style.visibility="visible"
-// })
+          }
+};
